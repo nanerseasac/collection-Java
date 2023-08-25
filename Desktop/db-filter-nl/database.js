25e5c338e087f1d5443d850e13a1c7ca1dcd2827
nanerseasac <renan.casaeses@gmail.com>
2023-08-25 00:13:08 -0300

    first commit

---
+++ Desktop/db-filter-nl/database.js
@@ -0,0 +1,80 @@
+const db = `
+C:\Users\Renan\Downloads\Telegram Desktop\arquivos\@txtbases private 2.txt (6 hits)
+	Line   135868: https://www.habbo.nl/registration jessevanloock@gmail.com:Heiliggraf1
+	Line   329228: https://www.habbo.nl warre.vranken@telenet.be:Wav25072006
+	Line   335715: https://www.habbo.nl/registration hekmat123@hotmail.nl:123321fbi
+	Line   502258: https://www.habbo.nl janvelde@gmail.com:KOMKOMMER10
+	Line   863988: https://www.habbo.nl/registration rensboogerd@live.nl:afc1900
+	Line   898100: https://www.habbo.nl brandonxox300@gmail.com:Brandon1995NL24VP271195
+  C:\Users\Renan\Downloads\Telegram Desktop\arquivos\@txtbases private 3.txt (8 hits)
+	Line   164669: https://www.habbo.nl r.stikkelman@hetnet.nl:koen1998
+	Line   311188: https://forum.imperialhabbo.nl/index.php -Gustavo:Baba2121
+	Line   320959: https://pahabbo.nl jonavethon:Jona00
+	Line   507850: https://www.habbo.nl prestigeqhd.7@gmail.com:Emiradem1
+	Line   540377: https://www.habbo.nl/registration yasar_kilic50@hotmail.com:zxcvbnm55
+	Line   612501: https://www.habbo.nl/account/password/resetIdentity/363dc5b5cf89a325ca2125890f94630323f5bc6a186de240d35e3a86124a11f2cf5a10ae29f6823e40c194aff03c66a24a14538d2128f0e5ee2ae99f90f8b14533fe052754c9adcdc711225669dd5cef1d576aa68a7292f50fe17844ca2312ca an-gelo-@hotmail.com:angelo.d
+	Line   680366: https://www.habbo.nl/community/photos linam123:chimen123456789
+	Line   874736: https://www.habbo.nl baby_desmet@outlook.com:Angelo
+  C:\Users\Renan\Downloads\Telegram Desktop\arquivos\@txtbases private 4.txt (11 hits)
+	Line    44413: https://www.habbo.nl dewinrawih@gmail.com:Gangwars78
+	Line   195579: https://www.habbo.nl husicmirza@gmail.com:AMAR12345
+	Line   297692: https://www.habbo.nl brangers.thomas@gmail.com:TB0305
+	Line   410201: https://www.habbo.nl/security_check_token frankverboomz@hotmail.com:?
+	Line   746161: https://www.habbo.nl/registration ruben.m.aipassa@gmail.com:None
+	Line   809412: https://www.habbo.nl tom-bos@hotmail.com:liekebos123456789
+	Line   819860: https://www.habbo.nl/registration hdylan995@gmail.com:boomer0977
+	Line   881422: https://www.habbo.nl stijn@southcamp.nl:Welkom2001
+	Line   882311: https://www.habbo.nl/registration isabel090301@gmail.com:konijn2001
+	Line   901191: https://www.habbo.nl/registration (https //www.habbo.nl/):merlijn.marquering@gmail.com:geheim123
+  C:\Users\Renan\Downloads\Telegram Desktop\arquivos\@txtbases private 5.txt (9 hits)
+	Line   123650: https://www.habbo.nl/registration jevaderweetzelf@gmail.com:popo9i9i
+	Line   187836: https://www.habbo.nl josescholte@hotmail.com:poppie2003
+	Line   196719: https://www.habbo.nl/registration noahpardoel2@gmail.com:Skatertje18
+	Line   337179: https://www.habbo.nl teamdd2018@gmail.com:TeamDD2018
+	Line   638386: https://www.habbo.nl/registration jonathan.denruyter@gmail.com:JonathaN//2259
+	Line   687269: https://www.habbo.nl mrvyran@gmail.com:U4qslzow
+	Line   719838: https://www.habbo.nl/login_popup marchand.arthur@gmail.com:arthur0404
+	Line   750110: https://www.habbo.nl/account/password/resetidentity/d2de69f2bd1488e93492e1c10e2d0193fcc055ac6adbadfab21664df82f17e889e97613c0cf4d7103785b8a19fc76586fc0c1cba6f5c445e90f478f077e325c99404fa9ff6f2acfce1bd87968fab2e0d9d560a1d7ee7f715f39d49bd31d84acd44ee370d2fa165cb16d4a07af07389e5 t.ooijevaar@quicknet.nl:moro9655
+	Line   774212: https://www.habbo.nl stenkuh2@gmail.com:Sten2006
+  C:\Users\Renan\Downloads\Telegram Desktop\arquivos\txtbaseurl 1408.txt (37 hits)
+	Line   163919: https://www.habbo.nl/registration:Taheisha_lake@hotmail.com:taheisha1993
+	Line   167169: https://www.habbo.nl/registration:taheisha_lake@hotmail.com:Taheisha21101994
+	Line   167171: https://www.habbo.nl/:taheisha.lake@hotmail.com:taheisha1994
+	Line   167172: https://www.habbo.nl/hotel:taheisha.lake@hotmai:taheisha1994
+	Line   167173: https://www.habbo.nl/hotel:taheisha_amanda@hotmail.com:taheisha1994
+	Line   167175: https://www.habbo.nl/hotel:taheisha-lake@hotmail.com:taheisha1994
+	Line   167177: https://www.habbo.nl/settings/email:taheisha.lake@outlook.com:taheisha1994
+	Line   167178: https://www.habbo.nl/:taheisha.amanda@hotmail.com:Codie
+	Line   167337: https://www.habbo.nl/registration:taheisha_amanda@outlook.com:Taheisha1994
+	Line   167338: https://www.habbo.nl/:taheisha_lake@outlook.com:taheisha1994
+	Line   167339: https://www.habbo.nl/settings/email:young_punanie@hotmail.com:Taheisha1994
+	Line   167483: https://www.habbo.nl/settings/email:taheisha-amanda@hotmail.com:Taheisha1994
+	Line   167485: https://www.habbo.nl/:taheisha_amanda@hotmail:Taheisha1994
+	Line   168301: https://forum.imperialhabbo.nl/index.php:KenyaGirly_:CodieLake21101994
+	Line   168304: https://www.imperialhabbo.nl/register:KenyaGirly_:CodieLake21101994
+	Line   168682: https://www.habbo.nl/:Huskylover-89@hotmail.com:riffienoobje123
+	Line   168683: https://www.habbo.nl/:mlssx.habbo@gmail.com:riffienoobje123
+	Line   173200: https://www.habbo.nl/community/article/25597/binnenkort-officiele-habbo-merchandise:taheisha_lake@hotmail.com:cherelda
+	Line   261122: https://www.habbo.nl/:marijnklonne@gmail.com:marijn2004
+	Line   261342: https://www.habbo.nl/:marijn200424@gmail.com:marijn2004
+	Line   948645: https://www.habbo.nl/:dani.heere@hotmail.com:dani2005
+	Line   950546: https://www.habbo.nl/:dani.heere21@gmail.com:dani2005
+	Line 1398928: https://www.habbo.nl/:wesleypeeters@live.nl:pokefan12-
+	Line 1412438: https://www.habbo.nl/:xx_sjo@hotmail.com:sjonalevi1
+	Line 1425848: https://www.habbo.nl/:crazystefannl@hotmail.com:banaan123
+	Line 1426786: https://www.habbo.nl/registration:stefanlans2001@hotmail.com:Banaan123
+	Line 1427126: https://www.habbo.nl/registration:thesapclan@hotmail.com:Banaan123
+	Line 3865398: https://www.habbo.nl/registration:vanderkortfinn@gmail.com:moesboy631
+	Line 4027961: https://www.habbo.nl/:erteminsta1@outlook.com:Stackers072
+	Line 4028230: https://www.habbo.nl/:googleweetali@outlook.com:Stackers072!!
+	Line 4028231: https://www.habbo.nl/:edi-vantol@hotmail.com:e210300
+	Line 4371924: https://www.habbo.nl/:gs_talha_42@live.nl:qwerty42
+	Line 4552975: https://www.habbo.nl/:luukfc@hotmail.nl:1Fcgfcgfcgfcg
+	Line 4555008: https://www.habbo.nl/registration:debumper@gmail.com:1Fcgfcgfcgfcg
+	Line 4556646: https://www.habbo.nl/:luukfc@hotmail.com:1Fcgfcgfcgfcg
+	Line 4567643: https://www.habbo.nl/:akwant@home.nl:1fcgfcg1fcgfcg
+	Line 4615349: https://www.habbo.nl/:timrutten4@Gmail.com:timingroesbeek1
+
+`
+
+module.exports = db
